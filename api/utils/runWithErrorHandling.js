b79ae8d53c9a701a207e11031780a2927a2d7645
donyol <danieltomlinson123@gmail.com>
2023-01-21 16:17:17 +0100

    update runWithErrorHandling in api

--- api/utils/runWithErrorHandling.js
+++ api/utils/runWithErrorHandling.js
@@ -8,6 +8,7 @@ const {
   NotFoundError,
   AuthError,
   DuplicityError,
+  SystemError,
 } = require("errors");
 
 function runWithErrorHandling(callback, res, logger) {
@@ -16,22 +17,23 @@ function runWithErrorHandling(callback, res, logger) {
       if (error instanceof DuplicityError)
         res.status(409).json({ error: error.message });
       else if (error instanceof NotFoundError || error instanceof AuthError)
-        res.status(401).json({ error: "wrong credentials" });
+        res.status(401).json({ error: error.message });
+      else if (error instanceof TypeError || error instanceof FormatError)
+        res.status(400).json({ error: error.message });
+      else if (
+        error instanceof JsonWebTokenError ||
+        error instanceof TokenExpiredError ||
+        error instanceof NotBeforeError
+      )
+        res.status(401).json({ error: "token not valid" });
+      else if (error instanceof SystemError)
+        res.status(500).json({ error: error.message });
       else res.status(500).json({ error: "system error" });
 
       logger.error(error);
     });
   } catch (error) {
-    if (error instanceof TypeError || error instanceof FormatError)
-      res.status(400).json({ error: error.message });
-    else if (
-      error instanceof JsonWebTokenError ||
-      error instanceof TokenExpiredError ||
-      error instanceof NotBeforeError
-    )
-      res.status(401).json({ error: "token not valid" });
-    else res.status(500).json({ error: "system error" });
-
+    res.status(500).json({ error: error.message });
     logger.error(error);
   }
 }
